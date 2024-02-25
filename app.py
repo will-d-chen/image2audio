b9ff4dde3116ca6394f959bad2f6c61b8de72f5d
ritu1412 <rt233@duke.edu>
2024-02-25 12:04:19 -0500

    Fixed image compressed

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 import google.generativeai as genai
 
@@ -27,12 +28,41 @@ def main():
     st.header('Find out how to recycle your items and where')
     st.write('Upload an image of the item you wish to recycle, and we\'ll tell you how!')
 
-    uploaded_image = st.file_uploader("",type=['png', 'jpg', 'jpeg'])
+    uploaded_image = st.file_uploader(
+        "",
+        type=["png", "jpg", "jpeg"],
+    )
 
     if uploaded_image is not None:
-        # Display a progress bar   
+        # Display a progress bar
         image = Image.open(uploaded_image)
-        st.image(image, caption='Your Uploaded Image', use_column_width=True)
+        user_uploaded_image = image.copy()
+        file_ext = str(uploaded_image.name.split(".")[-1]).lower().strip()
+        print(file_ext)
+
+        # Get image size in MB using os.stat
+        image = image.resize((image.width // 2, image.height // 2))
+        # Redice image quality
+        compressed_image_name = f"temp.{file_ext}"
+
+        image.save(
+            compressed_image_name,
+            "JPEG" if file_ext == "jpg" else file_ext.upper(),
+            quality=25,
+            optimize=True,
+            progressive=True
+        )
+        image = Image.open(compressed_image_name)
+        
+        # Print image size
+        image_size = os.stat(compressed_image_name).st_size / (1024 * 1024)
+        print(f"Compressed image size: {image_size} MB")
+
+        if image_size > 3.9:
+            st.error("Please upload an image less than 15MB.")
+            return
+        
+        st.image(user_uploaded_image, caption='Your Uploaded Image', use_column_width=True)
 
         # Get recycling instructions
         instructions = get_recycling_instructions(image, gemini_model)
@@ -47,6 +77,9 @@ def main():
             st.markdown(recycling_places, unsafe_allow_html=True)
         else:
             st.error("Could not get instructions for recycling this item.")
+
+        # Delete compressed_image_name file
+        os.remove(compressed_image_name)
     
 
 if __name__ == "__main__":
